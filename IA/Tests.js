863c943056c78eccd4a8130933ba9a0e8fee303b
Robson Luan <robsonluan273@hotmail.com>
2022-03-29 00:59:36 -0300

    Created new database connection class

--- IA/Tests.js
+++ IA/Tests.js
@@ -1,26 +1,12 @@
-import { database } from "./DataKeeper.js"
+import { database, db } from "./DataKeeper.js"
 import { ChatManager } from "./ChatManager.js"
 import { DataBase } from "./Utils.js"
 
 const data = new DataBase()
 //console.log('abcdjnaisbafbianda'.replaceAll('a', '0'))
-/*
+
 const fc = async function(){
-    await database.load()
-    await database.registerDiscs('123654', ['23', '14'], false)
-    await database.effetivate('123654')
-    await database.registerDiscs('123654', ['10', '35', '37', '41'], true)
-    await database.effetivate('123654')
+    let conn = await db.connect()
+    console.log(await conn.query(`insert into cadastro (numero) values ('123');`))
 }
 fc(2)
-*/
-let words = '&wrd*wrd1&wrd2'.split(/[&]|[*]/g).slice(1)
-let specs = '&wrd*wrd1&wrd2'.match(/[&]|[*]/g)
-let obj = {pres: [], nonp: []}
-words.forEach((i, k) => {
-    if(specs[k] === '&')
-        obj.pres.push(i)
-    else
-        obj.nonp.push(i)
-})
-console.log(obj)
