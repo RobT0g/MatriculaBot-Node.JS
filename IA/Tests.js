2b120960a24abd567fb61b75a55c93640cf448f3
Robson Luan <robsonluan273@hotmail.com>
2022-05-24 17:45:51 -0300

    changed database

--- IA/Tests.js
+++ IA/Tests.js
@@ -5,9 +5,7 @@ import { chat } from "./ChatFlow.js"
 import { StepStuff } from "./Msgs.js"
 
 async function test(){
-    await db.load()
-    await tags.actions['add_discs']({}, {tagInfo: ['', ['2', '7', '5']]}, '001')
-    //await database.effetivate('001')
+    
 }
 
 test()
